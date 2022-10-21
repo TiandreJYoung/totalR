f808c3190cbe9cdc70c486a1a3ff3f09113a3bd6
tiandre jackson <drashawnyoung84@gmail.com>
2022-10-21 13:36:35 -0500

    first commit

---
+++ total.js
@@ -0,0 +1,179 @@
+//string 
+//let firstvariable = "HelloWorld"
+//let secondvariable=undefined
+
+//firstvariable=1998
+//firstvariable=secondvariable
+//secondvariable="Hello Hello world"
+
+
+
+
+//booleans 
+//const a = 4; const b = 53; const c = 57; const d = 16; const e = 'Kevin';
+
+//console.log(a< b);//True
+ //console.log(c>d);//true
+  //console.log('Name'=== 'Name'); //True 
+
+
+
+
+
+
+
+//The Farm 
+//let animal = prompt("Name animal");
+//if (animal != "cow") {
+  //  console.log ("dont have this animal. Try cow")
+//} else {
+   // if (aminal==="cow"){
+   //     console.log ("mooooooo")
+   // }
+//}
+
+//Drivers Ed 
+
+//let currentAge;
+//currentAge= prompt("Whats your age");
+//if(currentAge>=16){
+//    console.log("here are the keys")
+//} else {
+ //   if (currentAge<16){
+ //       console.log("you are too young")
+ //   }
+//}
+
+//loops
+//for(let i2=0;i2<10;++1){
+   // console.log(i2)
+//}
+//for(let i3 =10; i3<=400; i3++){
+   // console.log(i3)
+//}
+//for(let i4 = 12; i4<=4000; i4+=3){
+ //   console.log(i4)
+//}
+
+//strings even 
+//for (let i5=0; 13<=100;i3++){
+ //if(i5 % 2==0){
+ //      console.log(`${i4}<--- is an even number`)
+ //  } else {
+ //   console.log(i4)
+  // }
+//}
+//high five
+
+//for(let i6=0; i6 <=100; i6++){
+ //   if(i6 >0 && !(i6 % 5)){
+   // console.log(`i found a ${i6} High five`)
+//} if (i6>0 && !(i6 % 3) ){
+//console.log(`i found a ${i6}three is a crowd`)
+//}
+//}
+
+//savings
+//let totalAccount=[1,2,3,4,5,6,7,8,9,10];
+//bankAccount=0;
+//totalAccount.forEach(element => {
+//    bankAccount+=element
+//});
+//console.log(bankAccount)
+//let bank2=0
+//for(i=0 ; i<10;i++){
+//    bank2+=i;
+//}
+
+//let bank3=0
+//for (leti8 =0;i8>=100;i++){
+  //  bank3 *=2
+//}
+//console.log(bank3)
+ 
+
+//Arrays 
+//const myArray8 = [5,10,500,20]
+
+
+
+
+
+//f
+//let bigsmalls =prompt ("enter number here");
+//if (bigsmall<100) {
+ //   console.log("little number")
+//}else{
+  //  (bigsmalls>=100){
+  //      console.log("big number")
+  //  }
+//}
+
+
+    //g
+   // let monkeyV= prompt ("enter number");
+   // if(monkeyV < 5){
+   //     console.log("little number")
+   // } else  {
+    //    (monkeyV > 10)
+    //    console.log ("bignumber")
+        
+  //  } else {    console.log("monkey"); }
+
+        //h
+
+     
+        //const kristynsCloset = [
+         //   "left shoe",
+          //  "cowboy boots",
+          //  "right sock",
+          //  "GA hoodie",
+          //  "green pants",
+          //  "yellow knit hat",
+          //  "marshmallow peeps"
+         // ];
+
+         // console.log(`Kristyn is rocking that ${kristynsCloset[2]} today`)
+         // kristynsCloset.splice(6,0, "raybans")
+         // kristynsCloset.splice(5,1, "stained knit hat")
+         // console.log(kristynsCloset)
+    
+         // const thomsCloset = [
+          //  [
+              // These are Thom's shirts
+           //   "grey button-up",
+           //   "dark grey button-up",
+           //   "light blue button-up",
+            //  "blue button-up",
+           // ],[
+              // These are Thom's pants
+           //   "grey jeans",
+           //   "jeans",
+           //   "PJs"
+           // ],[
+              // Thom's accessories
+           //   "wool mittens",
+             // "wool scarf",
+             // "raybans"
+           // ]
+          //];
+          //  thomsCloset[1].pop
+          //  thomsCloset[1].push ("footie pjs")
+//console.log (`thom is looking good in his ${thomsCloset[0][3]} and his ${thomsCloset[1][0]} with his ${thomsCloset[2][1]}`);
+
+//funtions
+
+//printCool= function(name){
+ //   this.name=name;
+//}
+//name=prompt("enter you name")
+//console.log (`${this.name} is cool`)
+
+//funtion Q
+
+//let calculateCube=(length,width,height,) =>{
+
+//let voulme=length*width*height;
+//console.log(`this cube voilme is ${voulme}`);
+//}
+//calculateCube(40,40,40)
